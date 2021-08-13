492fa2e2af37ef785f6c7ff20bd4dd39b3bc208e
Luca Silverentand <luca@onezero.company>
2021-08-13 20:58:13 +0200

    Implemented release creation.
    ci(publish) -> setup release creation workflow

--- .github/scripts/action-yaml.js
+++ .github/scripts/action-yaml.js
@@ -6,7 +6,7 @@ const content = readFileSync(file, 'utf-8');
 writeFileSync(
   file,
   content.replace(
-    'ghcr.io/onezerocompany/versioning:latest',
-    `ghcr.io/onezerocompany/versioning:${process.argv[2]}`
+    'ghcr.io/onezerocompany/versioning-action:latest',
+    `ghcr.io/onezerocompany/versioning-action:${process.argv[2]}`
   )
 );
